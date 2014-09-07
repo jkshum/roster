8ff8466b6dd18e0b84766214387134045237662c
Jacky Shum <jkshum@gmail.com>
2014-09-07 08:19:40 +0800

    monad environment

--- Main.hs
+++ Main.hs
@@ -12,31 +12,26 @@ data Person =
          , times :: Int
          } deriving (Show, Eq)
 
-persons = [(Person "Jacky" [6] 4),(Person "Timmy" [13] 2)]
+persons = [(Person "Jacky" [6] 4),(Person "Timmy" [13] 9)]
 
 dutyDates :: [Int]
 dutyDates = [6, 7, 13, 14, 20, 21, 27, 28]
 
-type Env  =  [(Maybe Person, Int)]
-type Context a = Reader Env a
+type Env  =  [(Person, Int)]
+type Context a = ReaderT Env Identity a
 
 schedule :: [Person] -> [Int] -> Context Env
 schedule _ [] = do env <- ask
                    return env
 schedule ps (d:ds) = do env <- ask
-                        local (const (env ++ [(pick env ps d, d)]))  (schedule ps ds)
+                        found <- pick ps d
+                        local (const $ env ++ [(found, d)]) $ schedule ps ds
 
-pick :: Env -> [Person] -> Int -> Maybe Person
-pick env ps d  
-  | res == [] = Nothing
-  | otherwise = Just $ head res
-  where res = [ p | p <- ps ,
-                not $ elem d (dates p),
-                exceeded env (Just p)]                
-                
-exceeded :: Env -> Maybe Person -> Bool
-exceeded env p = (times $ fromJust p) > length (filter (\x -> fst x == p) env)
-
-
+pick :: [Person] -> Int -> Context Person
+pick ps d = do env <-ask
+               let res = [p | p <- ps,
+                          not $ elem d (dates p),
+                          times p > length ( filter (\x -> fst x == p) env)] in
+                return $ head res
 
 
