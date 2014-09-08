d69226005f2cd3981f72538883dab5b79249414f
Jacky Shum <jkshum@gmail.com>
2014-09-08 15:21:26 +0800

    trying lens

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import Data.List
 import Data.Maybe
 import Control.Monad.Reader
 import Control.Monad.Identity
+import Control.Lens
 
 data Person =
   Person { name  :: String
@@ -19,16 +20,16 @@ dutyDates :: [Int]
 dutyDates = [6, 7, 13, 14, 20, 21, 27, 28]
 
 type Env  =  [(Maybe Person, Int)]
-type Context a = ReaderT Env Identity a
+type Plan a = ReaderT Env Identity a
 
-schedule :: [Person] -> [Int] -> Context Env
+schedule :: [Person] -> [Int] -> Plan Env
 schedule _ [] = do env <- ask
                    return env
 schedule ps (d:ds) = do env <- ask
                         found <- pick ps d
                         local (const $ env ++ [(found, d)]) $ schedule ps ds
 
-pick :: [Person] -> Int -> Context (Maybe Person)
+pick :: [Person] -> Int -> Plan (Maybe Person)
 pick ps d = do res <- filterM (\p -> evalState p d) ps
                case res of
                  [] -> return Nothing
@@ -38,7 +39,8 @@ pick ps d = do res <- filterM (\p -> evalState p d) ps
 -- evalJob :: Person -> Int -> Bool
 -- evalJob p d = not $ elem d (dates p)
 
-evalState :: Person -> Int -> Context Bool
+evalState :: Person -> Int -> Plan Bool
 evalState p d = do env <- ask 
                    let res = filter (\x -> fst x == Just p) env in
-                     return $ times p > (length $ res) && (not $ elem d (dates p))
+                     return $ times p > (length $ res) &&
+                     (not $ elem d (dates p))
