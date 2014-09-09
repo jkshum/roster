1d77367d6e328465a9d0c597333c9ae075b7b375
Jacky Shum <jkshum@gmail.com>
2014-09-09 17:43:08 +0800

    added job kind

--- Main.hs
+++ Main.hs
@@ -18,38 +18,51 @@ data Person =
          , kind :: [Kind]
          } deriving (Show, Eq)
 
-persons = [(Person "Jacky" [6] 4 2 [Sat, Sun, Morning]),
-           (Person "Timmy" [13] 2 2 [Sat, Morning]),
-           (Person "Lok" [21] 2 2) [Morning]]
+persons = [Person "Jacky" [6] 4 2 [Sat, Sun, Morning],
+           Person "Timmy" [13] 2 2 [Sat, Morning],
+           Person "Lok" [21] 2 2 [Morning]]
 
-dutyDates :: [Int]
-dutyDates = [6, 7, 13, 14, 20, 21, 27, 28]
+data Job =
+  Job {date :: Int
+      , jobKind :: Kind
+      } deriving (Show, Eq)
 
-type Env  =  [(Maybe Person, Int)]
+dutyDates :: [Job]
+dutyDates = [Job 6 Sat,
+             Job 7 Sun,
+             Job 7 Morning,
+             Job 13 Sat,
+             Job 14 Sun,
+             Job 14 Morning,
+             Job 20 Sat,
+             Job 21 Sun,
+             Job 21 Morning,
+             Job 27 Sat,
+             Job 28 Sun,
+             Job 28 Morning]
+            
+type Env  =  [(Maybe Person, Job)]
 type Plan a = ReaderT Env Identity a
 
-schedule :: [Person] -> [Int] -> Plan Env
+schedule :: [Person] -> [Job] -> Plan Env
 schedule _ [] = do env <- ask
                    return env
-schedule ps (d:ds) = do env <- ask
-                        found <- pick ps d
-                        local (const $ env ++ [(found, d)]) $ schedule ps ds
+schedule ps (j:js) = do env <- ask
+                        found <- pick ps j
+                        local (const $ env ++ [(found, j)]) $ schedule ps js
 
-pick :: [Person] -> Int -> Plan (Maybe Person)
-pick ps d = do res <- filterM (\p -> evalState p d) ps
+pick :: [Person] -> Job -> Plan (Maybe Person)
+pick ps j = do res <- filterM (\p -> evalState p j) ps
                case res of
                  [] -> return Nothing
                  _ -> return $ (Just $ head res)
 
-
--- evalJob :: Person -> Int -> Bool
--- evalJob p d = not $ elem d (dates p)
-
-evalState :: Person -> Int -> Plan Bool
-evalState p d = do env <- ask 
+evalState :: Person -> Job -> Plan Bool
+evalState p j = do env <- ask 
                    let res = filter (\x -> fst x == Just p) env in
                      return $ times p > (length $ res) &&
-                     (not $ elem d (dates p)) &&
+                     (not $ elem (date j) (dates p)) &&
+                     elem (jobKind j) (kind p) &&
                      (cooldown p < lastAssigned p env)
 
 lastAssigned :: Person -> Env -> Int
